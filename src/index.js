b94ce5bd7a5a3d928883aa0294caf57975f3e5ef
minhlong149 <minhlong9514@gmail.com>
2022-03-03 18:10:45 +0700

    FE project w/ SCSS & TypeScript

--- src/index.js
+++
@@ -1,1 +0,0 @@
-alert("Everything is working correctly");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/bundle.ts",
+  entry: "./src/index.ts",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -11,11 +11,8 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
           "style-loader",
-          // Translates CSS into CommonJS
           "css-loader",
-          // Compiles Sass to CSS
           "sass-loader",
         ],
       },
