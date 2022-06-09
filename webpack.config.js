c59a3001ec5f50598611fcd251a9f607d2ad9882
minhlong149 <minhlong9514@gmail.com>
2022-06-09 17:09:42 +0700

    Init project

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,35 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Homepage",
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.tsx?$/,
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.ts(x)?$/,
         use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
